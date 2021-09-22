97a668bd5c9bcbcc995839591ac20ebea269ece2
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2021-09-22 17:43:45 +0200

    Added mialb21 class, to print the cool name of mialb21

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        
+        // Let's print that name, shall we?
+        Mialb21 mialb21 = new Mialb21();
+        mialb21.printName();
    }
 }
---
+++ src/com/company/Mialb21.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Mialb21 {
+    public String name = "Mialb21 - The Mikkel";
+
+    public Mialb21() {
+        // The void is calling
+    }
+
+    public void printName() {
+        System.out.println(this.name);
+    }
+}
